26baf5fd8986e1c373f7f9e19b492060d054301a
Iris Lang <ilang@college.harvard.edu>
2022-09-17 17:23:23 -0400

    linked list append and erase

--- datareps2/ll1.cc
+++ datareps2/ll1.cc
@@ -2,8 +2,43 @@
 
 node* head = nullptr;
 
+void append(node* n){
+	node* curr = head;
+
+	if(head == nullptr){
+		head = n;
+		return;
+	}
+
+	while(curr->next != nullptr){
+		curr = curr->next;
+	}
+
+	curr->next = n;
+	n->prev = curr;
+	n->next = nullptr;
+
+}
+
+void erase(node* n){
+
+	if(n->next != nullptr){
+		n->next->prev = n->prev;
+	
+	if (n->prev) {
+        n->prev->next = n->next;
+    } 
+	
+	else {
+        head = n->next;
+    }
+	}
+}
+
 int main() {
 	printf("Hi! This program is a placeholder.\n");
 	printf("If you want to experiment with linked list code, either edit `ll1.cc` and `make`,\n");
 	printf("or make a file called `llNN.cc` and `make`.\n");
+
+
 }
