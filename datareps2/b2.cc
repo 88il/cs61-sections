c3bb63b523649d45cd733988656b5a55fba933cf
Iris Lang <ilang@college.harvard.edu>
2022-09-17 15:59:42 -0400

    played with char* buffer errors

--- datareps2/b2.cc
+++ datareps2/b2.cc
@@ -1,10 +1,20 @@
 #include <cstdio>
 
 const char* f(int) {
+    // WORKS!
+    // const char* buf = "example";
+    
+    // section example... does NOT work
     char buf[100] = "example";
+    
     return buf;
 }
 
 int main() {
+//    testing:
+//    const char* s = f(2);
+//    printf("%p\n", s);
+    
+
     printf("%s\n", f(2));
 }
